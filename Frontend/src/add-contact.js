56e0bf14b895e89728f599e001c4f5c77330af2d
kilfour <kilfour@gmail.com>
2026-05-19 15:59:53 +0200

    removing global state

--- Frontend/src/add-contact.js
+++ Frontend/src/add-contact.js
@@ -1,34 +1,23 @@
-import { getByClass, bindDialogForm } from "./_utils/ui.js";
+import { notConfigured, getByClass, bindDialogForm } from "./_utils/ui.js";
 import { createContact } from "./storage.js";
 
-let component = {};
-let elements = {};
-
 export function initializeAddContact(element) {
-    createComponent();
-    bindElements(element);
-    bindForm();
-    return component;
-}
 
-function createComponent() {
-    component = {
-        showDialog: a => elements.dialog.showModal(a),
-        onCreate: null
-    };
-}
-
-function bindElements(element) {
-    elements = {
+    const elements = {
         dialog: element,
         form: getByClass(element, 'addContactForm')
     };
-}
 
-function bindForm() {
+    const component = {
+        showDialog: () => elements.dialog.showModal(),
+        onCreate: () => notConfigured('add-contact', 'onCreate')
+    };
+
     bindDialogForm(
         elements.dialog,
         elements.form,
         a => createContact(a),
         () => component.onCreate());
+
+    return component;
 }
--- Frontend/src/edit-contact.js
+++ Frontend/src/edit-contact.js
@@ -1,46 +1,35 @@
-import { getByClass, bindDialogForm } from "./_utils/ui.js";
+import { notConfigured, getByClass, bindDialogForm } from "./_utils/ui.js";
 import { updateContact } from "./storage.js";
 
-let component = {};
-let elements = {};
-let contact = {};
 
 export function initializeEditContact(element) {
-    createComponent();
-    bindElements(element)
-    bindForm();
-    return component;
-}
 
-function createComponent() {
-    component = {
-        showDialog: a => showDialog(a),
-        onConfirmed: null
-    };
-}
+    let contact = null;
 
-function bindElements(element) {
-    elements = {
+    const elements = {
         dialog: element,
         form: getByClass(element, 'addContactForm'),
         name: getByClass(element, "name"),
         email: getByClass(element, "email"),
         phone: getByClass(element, "phone"),
     };
-}
 
-function bindForm() {
+    const component = {
+        showDialog(contactToShow) {
+            contact = contactToShow;
+            elements.name.value = contact.name;
+            elements.email.value = contact.email;
+            elements.phone.value = contact.phone;
+            elements.dialog.showModal();
+        },
+        onConfirmed: () => notConfigured('edit-contact', 'onConfirmed')
+    };
+
     bindDialogForm(
         elements.dialog,
         elements.form,
         a => { a.id = contact.id; contact = a; updateContact(a); },
         () => component.onConfirmed(contact));
-}
 
-function showDialog(contactToShow) {
-    contact = contactToShow;
-    elements.name.value = contact.name;
-    elements.email.value = contact.email;
-    elements.phone.value = contact.phone;
-    elements.dialog.showModal();
+    return component;
 }
